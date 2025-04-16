871983cb5e566666d0f6c5b368d2d95e3e8b1ae7
nkb0401 <khoabang.nguyen@gmail.com>
2025-04-16 20:03:32 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,92 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config();
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Dùng thư mục public để phục vụ frontend
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route xử lý khi nhấn nút RUN (API call)
+app.post('/askOPENAPI', async (req, res) => {
+ const userMessage = req.body.message;
+ const payload = {
+  model: "gpt-4o-mini",
+  store: true,
+  messages: [{ role: "user", content: userMessage }]
+ };
+
+ const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+ };
+ console.log("📤 Đang gửi request đến OpenAI:");
+ console.log("👉 URL:", 'https://api.openai.com/v1/chat/completions');
+ console.log("👉 Headers:", headers);
+ console.log("👉 Body:", JSON.stringify(payload, null, 2));
+ try {
+ 
+  const response = await axios.post(
+   'https://api.openai.com/v1/chat/completions',
+   payload,
+   { headers }
+  );
+  const reply = response.data.choices?.[0]?.message?.content;
+  res.json({ reply });
+ //console.log("👉 Body:",response.data.choices?.[0]?.message?.content)
+
+ } catch (err) {
+  console.error('API error:', err.response?.data || err.message);
+  res.status(500).json({ error: "Lỗi khi gọi OpenAI API" });
+ }
+});
+
+app.post('/askGROQAPI', async (req, res) => {
+ const userMessage = req.body.message;
+ const payload = {
+  model: "llama-3.3-70b-versatile",
+  messages: [{ role: "user", content: userMessage }]
+ };
+
+ const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${process.env.GROQ_API_KEY}`
+ };
+ console.log("📤 Đang gửi request đến OpenAI:");
+ console.log("👉 URL:", 'https://api.groq.com/openai/v1/chat/completions');
+ console.log("👉 Headers:", headers);
+ console.log("👉 Body:", JSON.stringify(payload, null, 2));
+ try {
+ 
+  const response = await axios.post(
+   'https://api.groq.com/openai/v1/chat/completions',
+   payload,
+   { headers }
+  );
+
+  const reply = response.data.choices?.[0]?.message?.content;
+  res.json({ reply });
+ //console.log("👉 Body:",response.data.choices?.[0]?.message?.content)
+
+ } catch (err) {
+  console.error('API error:', err.response?.data || err.message);
+  res.status(500).json({ error: "Lỗi khi gọi OpenAI API" });
+ }
+});
+
+// Mặc định: nếu route không khớp => trả về index.html
+app.get('/', (req, res) => {
+ res.sendFile(path.join(__dirname, 'public','index.html'));
+
+});
+
+app.listen(PORT, () => {
+ console.log(`✅ Server đang chạy: http://localhost:${PORT}`);
+});
